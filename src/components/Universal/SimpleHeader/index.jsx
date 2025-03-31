497d8ad12ef1be5e8041e26b9cf47018460f8cea
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-03-31 16:18:52 -0300

    feat: add kubo icon to the header

--- src/components/Universal/SimpleHeader/index.jsx
+++ src/components/Universal/SimpleHeader/index.jsx
@@ -1,6 +1,12 @@
+import "./styles.css"
+import KuboIcon from "../../../assets/icons/Universal/kubo-main-icon.svg"
+
 export function SimpleHeader(){
     return(
         <header>
+            <div className="icon-container">
+                <img src={KuboIcon} alt="Kubo icon" className="kubo-icon" />
+            </div>
             <h1>KUBO</h1>
         </header>
     )
--- src/views/LoginPage/index.jsx
+++ src/views/LoginPage/index.jsx
@@ -1,9 +1,10 @@
 import "./styles.css"
+import { SimpleHeader } from "../../components/Universal/SimpleHeader"
 
 export default function LoginPage(){
     return(
         <div className="container">
-            <h1>Hello World</h1>
+            <SimpleHeader />
         </div>
     )
 }
