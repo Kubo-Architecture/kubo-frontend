1a0be2e4d5324977c3f031621a70f01773748f11
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-03-31 09:30:31 -0300

    feat: implement router inicial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-
-import { useState } from 'react';
 import './App.css';
-import Tela from './Tela'; // Importando o componente
-
-import { useState } from 'react'
+import Tela from './Tela';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
---
+++ src/routes/routes.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+export default function Rotas() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* <Route path="/" Component={LoginPage} /> */}
+            </Routes>
+        </BrowserRouter>
+    );
+}
