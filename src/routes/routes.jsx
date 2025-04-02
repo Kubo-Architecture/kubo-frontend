4b8c1014ba7a9506344e2a977396e015536bee38
lucas19fonseca <lucas19fonseca@gmail.com>
2025-04-02 13:49:05 -0300

    home

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "../views/LoginPage"
 import ErrorPage from "../views/ErrorPage";
+import Home from "../views/home";
 
 
 export default function Rotas() {
@@ -10,6 +11,7 @@ export default function Rotas() {
                 <Route path="/" element={<h1>Ola mundo</h1>} />
                 <Route path="/login" element={<LoginPage/>} />
                 <Route path="/error/:errorCode" element={<ErrorPage />} />
+                <Route path="/home" element={<Home/>} />
             </Routes>
         </BrowserRouter>
     );
