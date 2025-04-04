381ebd4fc6b9f63325690424c4337c7fcc1f681a
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-04-04 09:24:42 -0300

    feat: implement router structure for register

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "../views/LoginPage"
 import ErrorPage from "../views/ErrorPage";
+import Register from "../views/Register";
 // import Home from "../views/home";
 
 
@@ -11,6 +12,7 @@ export default function Rotas() {
                 <Route path="/" element={<h1>Ola mundo</h1>} />
                 <Route path="/login" element={<LoginPage/>} />
                 <Route path="/error/:errorCode" element={<ErrorPage />} />
+                <Route path="/register" element={<Register />} />
                 {/* <Route path="/home" element={<Home/>} /> */}
             </Routes>
         </BrowserRouter>
