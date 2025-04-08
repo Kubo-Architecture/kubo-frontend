4cde08107e1088022b8814f949da22130f8443d1
lucas19fonseca <lucas19fonseca@gmail.com>
2025-04-07 21:03:00 -0300

    branch-body

---
+++ src/components/Home/HomeBody/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css"
+
+
+
+export default function HomeBody(){
+    return(
+      <body>
+        <h1>dada</h1>
+      </body>
+    )
+}
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -5,6 +5,7 @@ export default function Home(){
     return(
         <div className="home-container">
             <HomeHeader/>
+            <HomeBody/>
         </div>
     )
 }
