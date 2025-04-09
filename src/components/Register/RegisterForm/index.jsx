7b76fadc2e3a545050448b416a6c9d6c00de096f
Gabriel Lima <gabrilima2004@gmail.com>
2025-04-09 12:30:34 -0300

    feat: Creating base files for the Register Page

---
+++ src/components/Register/RegisterForm/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css"
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import InputField from '../../Universal/InputField';
+
+const RegisterForm = () => {
+
+};
+
+export default RegisterForm;
---
+++ src/views/Register/index.jsx
@@ -0,0 +1,10 @@
+import "./styler.css"
+//import RegisterForm from "../../components/Register/RegisterForm"
+
+export default function RegisterPage(){
+    return(
+        <>
+        
+        </>
+    )
+}
