145d69d0c86bf046f512644f6c1970067528079a
lucas19fonseca <lucas19fonseca@gmail.com>
2025-04-24 09:10:33 -0300

    adc-tailwind

--- src/components/Home/HomeBody/index.jsx
+++ src/components/Home/HomeBody/index.jsx
@@ -3,11 +3,11 @@ import ImageNovidades from "../../../assets/icons/Universal/ImageNovidades.svg";
 import ImgFaixa from "../../../assets/icons/Universal/ImgFaixa.svg";
 import ImgOnda from "../../../assets/icons/Universal/ImgOnda.svg";
 import ImgPredio from "../../../assets/icons/Universal/ImgPredio.svg";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function HomeBody(){
     return(
-      <body>
+      <div className="body-container">
+    
         <div className="container-white">
           <section class="container-aviso">
             <h1 class="title-kubo">Kubo</h1>
@@ -26,7 +26,7 @@ export default function HomeBody(){
                 <img src={ImgPredio} alt="Planta arquitetônica" className="img ImgPredio" />
               </div>
           </div>
-
+          
       <section class="container-black">
         <p class="text-container">
           + de <strong class="color-1000">1000</strong> projetos documentados
@@ -38,15 +38,15 @@ export default function HomeBody(){
         <div class="conteudo-novidades">
           <h1 class="novidadeh1">Novidades</h1>
           <div class="novidade-textos">
-            <p>Agora, além de explorar</p>
-            <p>milhares de estruturas </p>
-            <p><b>incríveis</b>,você pode memorizar</p>
-            <p>conceitos,técnicas e estilos de</p>
-            <p> um jeito <b>interativo</b> e <b>eficaz</b>.</p>
+            <p>Agora, além de explorar
+            milhares de estruturas 
+            <b>incríveis</b>,você pode memorizar
+            conceitos,técnicas e estilos de
+             um jeito <b>interativo</b> e <b>eficaz</b>.</p>
           </div>
           <a href="#" class="btn-flashcard">Descubra os flashcards</a>
         </div>
       </div> 
-      </body>
+      </div>
     )
 }
