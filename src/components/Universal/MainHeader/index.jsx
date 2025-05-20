30872e81a7a73bcbaa1ed5d9d6f31ac136794e7f
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-05-20 19:59:40 -0300

    feat: implement nav iconsand made them clickable

--- src/components/Universal/MainHeader/index.jsx
+++ src/components/Universal/MainHeader/index.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import "./styles.css";
 import MenuLogo from "../../../assets/icons/Universal/HamburguerIcon.svg";
 import KuboLogo from "../../../components/Universal/KuboLogo"
+import HeartIcon from "../../../assets/icons/Universal/emptyheart.svg"
+import ProfileIcon from "../../../assets/icons/Universal/defaultUserPhoto.svg"
+import SettingsIcon from "../../../assets/icons/Universal/settingsbasic.svg"
 
 export default function IconWithPanel() {
   const [open, setOpen] = useState(false);
@@ -28,7 +31,17 @@ export default function IconWithPanel() {
         </div>
       </div>
       <KuboLogo/>
-      <div className="nav"></div>
+      <div className="nav-bar">
+      <div className="icon-button">
+        <img src={HeartIcon} alt="Menu-icon" id="heart-icon" className="menu-icon"/>
+      </div>
+      <div className="icon-button">
+        <img src={SettingsIcon} alt="Menu-icon" id="settings-icon" className="menu-icon"/>
+      </div>
+      <div className="icon-button">
+        <img src={ProfileIcon} alt="Menu-icon" id="profile-icon" className="menu-icon"/>
+      </div>
+      </div>
     </header>
   );
 }
