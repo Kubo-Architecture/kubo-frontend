a1c87beac852c30c7487e48bde80514db3ba22b1
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-05-20 18:32:27 -0300

    feat: create menu button and made functional

--- src/components/Universal/MainHeader/index.jsx
+++ src/components/Universal/MainHeader/index.jsx
@@ -1,8 +1,27 @@
-import "./styles.css"
-export default function MainHeader(){
-    return(
-        <header>
-            ola mundo ola mundo
-        </header>
-    )
+import React, { useState } from "react";
+import "./styles.css";
+import MenuLogo from "../../../assets/icons/Universal/HamburguerIcon.svg"
+
+export default function IconWithPanel() {
+  const [open, setOpen] = useState(false);
+
+  const togglePanel = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <header>
+        <div className="icon-button" onClick={togglePanel}>
+            <img src={MenuLogo} alt="Menu icon" className="menu-icon" />
+        </div>
+
+      {open && (
+        <div className="side-panel">
+          <p>Conteúdo do painel</p>
+          <button onClick={togglePanel}>Fechar</button>
+        </div>
+        
+      )}
+    </header>
+  );
 }
