8861b2b2d14222109627a56e6771fb47040a3f3e
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-05-20 18:59:54 -0300

    feat: add animation for panel opening and close

--- src/components/Universal/MainHeader/index.jsx
+++ src/components/Universal/MainHeader/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./styles.css";
-import MenuLogo from "../../../assets/icons/Universal/HamburguerIcon.svg"
+import MenuLogo from "../../../assets/icons/Universal/HamburguerIcon.svg";
 
 export default function IconWithPanel() {
   const [open, setOpen] = useState(false);
@@ -11,30 +11,27 @@ export default function IconWithPanel() {
 
   return (
     <header>
-        <div className="icon-button" onClick={togglePanel}>
-            <img src={MenuLogo} alt="Menu icon" className="menu-icon" />
-        </div>
+      <div className="icon-button" onClick={togglePanel}>
+        <img src={MenuLogo} alt="Menu icon" className="menu-icon" />
+      </div>
 
-      {open && (
-        <div className="side-panel">
-            <div className="item">
-                <p>Geral</p>
-            </div>
-            <div className="item">
-                <p>Conta</p>
-            </div>
-            <div className="item">
-                <p>Planos e upgrades</p>
-            </div>
-            <div className="item">
-                <p>Inteface</p>
-            </div>
-            <div className="item">
-                <p>Privacidade</p>
-            </div>
+      <div className={`side-panel ${open ? "open" : ""}`}>
+        <div className="item">
+          <p>Geral</p>
         </div>
-        
-      )}
+        <div className="item">
+          <p>Conta</p>
+        </div>
+        <div className="item">
+          <p>Planos e upgrades</p>
+        </div>
+        <div className="item">
+          <p>Inteface</p>
+        </div>
+        <div className="item">
+          <p>Privacidade</p>
+        </div>
+      </div>
     </header>
   );
 }
