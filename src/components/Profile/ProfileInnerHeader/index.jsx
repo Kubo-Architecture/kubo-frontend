13043b15120c673df4408d01cf12ea271894947f
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-05-25 14:33:19 -0300

    feat: implement photo user photo container

--- src/components/Profile/ProfileInnerHeader/index.jsx
+++ src/components/Profile/ProfileInnerHeader/index.jsx
@@ -1,7 +1,8 @@
 export default function ProfileInnerHeader(){
     return(
-        <div className="h-[100px] w-screen bg-[#C4C4C4]">
-
+        <div className="h-[150px] w-screen bg-[#C4C4C4] flex px-[20px] relative">
+            <div className="h-[150px] w-[150px] rounded-full bg-[url('src/assets/Profile/defaultProfile.svg')] bg-cover border-white border-3 absolute top-15">
+            </div>
         </div>
     )
 }
