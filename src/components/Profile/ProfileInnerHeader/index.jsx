c4cc71449bd1accbc62055c4935fef9197d5918b
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-05-25 16:04:33 -0300

    refac: ajust edit icon position

--- src/components/Profile/ProfileInnerHeader/index.jsx
+++ src/components/Profile/ProfileInnerHeader/index.jsx
@@ -1,7 +1,7 @@
 export default function ProfileInnerHeader(){
     return(
         <div className="h-[150px] w-screen bg-[#C4C4C4] flex px-[20px] relative justify-end">
-            <div className="h-[150px] w-[150px] rounded-full bg-[url('src/assets/Profile/defaultProfile.svg')] bg-cover border-white border-3 absolute left-[20px] top-15">
+            <div className="h-[160px] w-[160px] rounded-full bg-[url('src/assets/Profile/defaultProfile.svg')] bg-cover border-white border-3 absolute left-[20px] top-13">
             </div>
             <div className="w-[30px] h-[30px] bg-white rounded-full flex justify-center items-center my-[10px]">
                 <a href="#">
