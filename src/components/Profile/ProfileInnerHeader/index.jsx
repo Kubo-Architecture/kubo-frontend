2d53fef259921fe6204e1d718359ebfc52c909b9
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-05-25 15:08:28 -0300

    feat: add edit icon

--- src/components/Profile/ProfileInnerHeader/index.jsx
+++ src/components/Profile/ProfileInnerHeader/index.jsx
@@ -1,7 +1,12 @@
 export default function ProfileInnerHeader(){
     return(
-        <div className="h-[150px] w-screen bg-[#C4C4C4] flex px-[20px] relative">
-            <div className="h-[150px] w-[150px] rounded-full bg-[url('src/assets/Profile/defaultProfile.svg')] bg-cover border-white border-3 absolute top-15">
+        <div className="h-[150px] w-screen bg-[#C4C4C4] flex px-[20px] relative justify-end">
+            <div className="h-[150px] w-[150px] rounded-full bg-[url('src/assets/Profile/defaultProfile.svg')] bg-cover border-white border-3 absolute left-[20px] top-15">
+            </div>
+            <div className="w-[30px] h-[30px] bg-white rounded-full flex justify-center items-center my-[10px]">
+                <a href="#">
+                    <img src="src/assets/Profile/pen.svg" alt="" />
+                </a>
             </div>
         </div>
     )
