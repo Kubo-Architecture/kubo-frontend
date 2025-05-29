c88bbe91162fc4130d061a4fcee9042532f50124
lucas19fonseca <lucas19fonseca@gmail.com>
2025-05-29 09:45:20 -0300

    fix: solve some merge problem

---
+++ src/views/LandingPage/index.jsx
@@ -0,0 +1,11 @@
+import LandingPageHeader from "../../components/LandingPage/LandingPageHeader"
+import LandingPageBody from "../../components/LandingPage/LandingPageBody"
+
+export default function LandingPage() {
+    return (
+        <div className="h-screen">
+            <LandingPageHeader />
+            <LandingPageBody />
+        </div>
+    )
+}
