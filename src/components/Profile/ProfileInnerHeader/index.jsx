dac953d9525cd1327d95a2f0d52363b9ed481560
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-06-03 19:51:02 -0300

    feat: implement profile photo if exist

--- src/components/Profile/ProfileInnerHeader/index.jsx
+++ src/components/Profile/ProfileInnerHeader/index.jsx
@@ -1,13 +1,19 @@
-export default function ProfileInnerHeader(){
-    return(
+export default function ProfileInnerHeader(props) {
+    const profileImageUrl = props.photoUrl?.trim()
+        ? `url(${props.photoUrl})`
+        : `url('src/assets/Profile/defaultProfile.svg')`;
+
+    return (
         <div className="h-[150px] w-screen bg-[#C4C4C4] flex px-[20px] relative justify-end">
-            <div className="h-[160px] w-[160px] rounded-full bg-[url('src/assets/Profile/defaultProfile.svg')] bg-cover border-white border-3 absolute left-[20px] top-13">
-            </div>
+            <div
+                className="h-[160px] w-[160px] rounded-full bg-cover border-white border-3 absolute left-[20px] top-13"
+                style={{ backgroundImage: profileImageUrl }}
+            />
             <div className="w-[30px] h-[30px] bg-white rounded-full flex justify-center items-center my-[10px]">
                 <a href="#">
                     <img src="src/assets/Profile/pen.svg" alt="" />
                 </a>
             </div>
         </div>
-    )
+    );
 }
