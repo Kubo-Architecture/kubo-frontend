12027ec8a3f01183a88d4192b58f8b5982620339
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-06-04 14:45:33 -0300

    feat: create a father div for banner settings panel

--- src/components/Profile/BannerSettings/index.jsx
+++ src/components/Profile/BannerSettings/index.jsx
@@ -1,5 +1,7 @@
 export default function BannerSettings(){
     return(
-        <div className="h-full w-full bg-white"></div>
+        <div className="h-screen w-screen bg-[#000000b4]">
+            <div className="h-full w-[90%] bg-white"></div>
+        </div>
     )
 }
