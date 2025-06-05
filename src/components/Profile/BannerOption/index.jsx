c040196a0a0e37c183a12b1f7499e5b7cf48a81d
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-06-04 22:35:53 -0300

    feat: add two more banners

---
+++ src/components/Profile/BannerOption/index.jsx
@@ -0,0 +1,9 @@
+export default function BannerOption(props){
+    return(
+        <div className="h-[150px] w-[300px] rounded-[10px] hover: cursor-pointer"
+        style={{background: props.background}}
+        >
+            
+        </div>
+    )
+}
--- src/components/Profile/BannerSettings/index.jsx
+++ src/components/Profile/BannerSettings/index.jsx
@@ -1,8 +1,14 @@
+import BannerOption from "../BannerOption"
 export default function BannerSettings({ onClose }) {
     return (
         <div className="h-screen w-screen bg-[#000000b4] flex justify-center items-end">
-            <div className="h-[calc(100vh-80px)] w-full max-w-[800px] bg-white relative shadow-lg p-6">
-                <h2 className="text-xl font-semibold mb-4">Configurar banner</h2>
+            <div className="h-full w-full max-w-[800px] bg-white relative shadow-lg p-6"> 
+                <h2 className="text-xl font-semibold mb-4">Escolha o seu banner</h2>
+                <div className="flex pt-[50px] h-full w-full justify-start items-center gap-4 flex-col">
+                    <BannerOption background="green"/>
+                    <BannerOption background="red"/>
+                </div>
+                
             </div>
         </div>
     )
