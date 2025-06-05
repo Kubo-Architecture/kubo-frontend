c9a76b361231484c6e2f252cc853547b4ed81731
lucas19fonseca <lucas19fonseca@gmail.com>
2025-06-04 22:31:30 -0300

    ajuste nome kubo

--- src/components/Universal/KuboLogo/index.jsx
+++ src/components/Universal/KuboLogo/index.jsx
@@ -5,7 +5,7 @@ export default function Logocontainer() {
     <div className="flex items-center relative justify-center">
       <a href="/" className="flex items-center gap-1">
         <img src={KuboIcon} alt="Ícone da Kubo" className="w-8 h-8" />
-        <h1 className="text-2xl font-montserrat font-normal pt-1">Kubo</h1>
+        <h1 className="text-2xl font-montserrat font-normal pt-1 hidden md:block">Kubo</h1>
       </a>
     </div>
   );
