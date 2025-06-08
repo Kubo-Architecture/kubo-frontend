7bb4b1d566c8620598febe82a3db5414826c1fbe
lucas19fonseca <lucas19fonseca@gmail.com>
2025-06-08 17:40:08 -0300

    adc comentarios

--- src/components/LandingPage/LandingPageBody/index.jsx
+++ src/components/LandingPage/LandingPageBody/index.jsx
@@ -5,6 +5,8 @@ import ImgPredio from "../../../assets/icons/Universal/ImgPredio.svg";
 export default function HomeBody() {
   return (
     <main>
+      {/* Kubo: O maior banco de dados de arquitetura do país*/}
+
       <div className="overflow-hidden font-monospace min-w-[422px] md:min-w-[960px] lg:min-w-[1090px] ">
         <div className="grid grid-cols-2 ">
           <div className="px-9 pt-10 h-162 md:px-20">
@@ -14,6 +16,7 @@ export default function HomeBody() {
             </div>
           </div>
 
+          {/*imagens*/}
           <div className="flex flex-col relative h-[100vh] ">
             <img
               src={ImgFaixa}
@@ -28,10 +31,12 @@ export default function HomeBody() {
             <img
               src={ImgPredio}
               alt="Imagem predio"
-              className="w-60 h-40 object-cover rounded-xl absolute top-88 left-5 md:top-85 md:-left-30 md:h-60 md:w-auto  2xl:w-auto 2xl:h-80 2xl:top-125 2xl:left-5"
+              className="w-60 h-40 object-cover rounded-xl absolute top-88 left-5 md:top-85 md:-left-30 md:h-60 md:w-auto 2xl:w-auto 2xl:h-80 2xl:top-125 2xl:left-5"
             />
           </div>
         </div>
+         
+         {/*container projetos*/}
         <div className="bg-black w-full h-20 flex items-center justify-center">
           <p className="text-white text-2xl font-light 2xl:text-4xl">
             + de <span className="text-blue-400">1000</span> projetos documentados
