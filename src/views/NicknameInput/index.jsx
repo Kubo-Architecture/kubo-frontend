22a4e064ad43518bee6d4ea9e1d0cd3e302b1468
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-06-10 09:29:12 -0300

    feat: ajust some mesures

--- src/views/NicknameInput/index.jsx
+++ src/views/NicknameInput/index.jsx
@@ -2,10 +2,11 @@ import KuboIcon from "../../assets/icons/Universal/kubo-main-icon.svg"
 
 export default function NicknameInput(){
     return(
-        <div className="h-screen">
+        <div className="h-screen w-full">
             <header className="h-[80px] flex justify-center items-center">
                 <img className="h-[30px]" src={KuboIcon} alt="kubo icon" />
             </header>
+            <div className="h-full"></div>
         </div>
     )
 }
