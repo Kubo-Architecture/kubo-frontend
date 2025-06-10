c853bc6e56b94e6493cf6cb70d0a905bf1055f15
Bruno César <bcgmeireles@gmail.com>
2025-06-10 12:16:21 -0300

    feat: implements full gallery section

--- src/views/ProjectPage/index.jsx
+++ src/views/ProjectPage/index.jsx
@@ -5,8 +5,8 @@ export default function ProjectPage() {
     return (
         <>
         <IconWithPanel />
-        <div className="flex flex-col w-full min-h-[calc(100vh-5rem)] bg-red-500">
-            <div className="flex w-full h-12 justify-end items-center px-5 gap-10 bg-white">
+        <div className="flex flex-col w-full min-h-[calc(100vh-5rem)]">
+            <div className="flex w-full h-12 justify-end items-center px-5 gap-10">
                 <div className="flex h-full items-center gap-2">
                     <span>Postagem verificada</span>
                     <div className="w-5 h-5 bg-gray-100"></div>
@@ -21,14 +21,14 @@ export default function ProjectPage() {
                 <img className="w-full h-full object-cover" src={ProjectImage}/>
             </div>
 
-            <div className="flex w-full h-12 justify-end items-center px-5 gap-10 bg-white">
+            <div className="flex w-full h-12 justify-end items-center px-5 gap-10">
                 <div className="flex h-full items-center gap-2">
                     <div className="w-5 h-5 bg-gray-100"></div>
                     <span className="font-light text-sm">João Figueiras Lima</span>
                 </div>
             </div>
 
-            <div className="flex flex-col w-full px-12 bg-white">
+            <div className="flex flex-col w-full px-12">
                 <span className="text-4xl font-medium">Mansão dos Arcos</span>
                 <span>por Lele</span>
                 <div className="flex gap-2">
@@ -36,7 +36,8 @@ export default function ProjectPage() {
                     <div className="w-5 h-5 bg-gray-100"></div>
                 </div>
 
-                <span className="text-sm text-[#4f4f4f] mt-10">
+                <span className="text-sm text-[#4f4f4f] mt-10 text-justify md:text-start
+                                md:text-lg">
                     A Mansão dos Arcos é uma construção arquitetônica que se destaca por sua
                     grandiosidade e elementos clássicos, muitas vezes associados a estilos  
                     como o neoclássico, barroco ou até mesmo revivalista, dependendo da  
@@ -52,7 +53,7 @@ export default function ProjectPage() {
                 </span>
             </div>
 
-            <div className="grid grid-cols-2 gap-y-5 p-12 text-xl bg-white">
+            <div className="grid grid-cols-2 gap-y-5 p-12 text-xl">
                 <div className="flex items-center gap-3">
                     <div className="w-10 h-10 bg-gray-100"></div>
                     <div className="flex flex-col">
@@ -98,6 +99,28 @@ export default function ProjectPage() {
                     <div className="flex flex-col">
                         <span>Área construída</span>
                         <span className="font-light text-xs">20.232km²</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex flex-col p-12">
+                <span className="text-3xl font-medium mb-5">Galeria</span>
+                <div className="grid grid-cols-1 sm:grid-rows-2 sm:grid-cols-3 gap-1
+                                lg:grid-rows-3">
+                    <div className="rounded-sm sm:row-span-2 lg:row-span-3">
+                        <img className="w-full h-full object-cover rounded-sm" src={ProjectImage}/>
+                    </div>
+                    <div className="rounded-sm">
+                        <img className="w-full h-full object-cover rounded-sm" src={ProjectImage}/>
+                    </div>
+                    <div className="rounded-sm lg:row-span-2">
+                        <img className="w-full h-full object-cover rounded-sm" src={ProjectImage}/>
+                    </div>
+                    <div className="rounded-sm lg:row-span-2">
+                        <img className="w-full h-full object-cover rounded-sm" src={ProjectImage}/>
+                    </div>
+                    <div className="rounded-sm">
+                        <img className="w-full h-full object-cover rounded-sm" src={ProjectImage}/>
                     </div>
                 </div>
             </div>
