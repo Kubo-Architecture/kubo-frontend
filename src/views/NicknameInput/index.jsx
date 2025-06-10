c1d8820bcba88c4302916170b2bd6b8feabcd073
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-06-10 09:16:49 -0300

    feat: add kubo icon to the header

--- src/views/NicknameInput/index.jsx
+++ src/views/NicknameInput/index.jsx
@@ -1,7 +1,11 @@
+import KuboIcon from "../../assets/icons/Universal/kubo-main-icon.svg"
+
 export default function NicknameInput(){
     return(
-        <div className="h-screen ">
-            hello world
+        <div className="h-screen">
+            <header className="h-[80px] flex justify-center items-center">
+                <img className="h-[30px]" src={KuboIcon} alt="kubo icon" />
+            </header>
         </div>
     )
 }
