0321f26c1edcc0bda4bc48896e1e13737b72a6fa
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-06-10 18:46:47 -0300

    feat: add inner header responsiveness

--- src/components/Profile/ProfileInnerHeader/index.jsx
+++ src/components/Profile/ProfileInnerHeader/index.jsx
@@ -8,7 +8,7 @@ export default function ProfileInnerHeader(props) {
 
   return (
     <div
-      className="h-[150px] w-screen  min-w-[380px] flex px-[20px] relative justify-end"
+      className="h-[150px] md:h-[200px] lg:h-[300px] xl:h-[350px] w-screen  min-w-[380px] flex px-[20px] md:px-[30px] relative justify-end"
       style={{
         backgroundColor: props.banner ? undefined : "#C4C4C4",
         backgroundImage: props.banner ? `url(${props.banner})` : undefined,
@@ -17,11 +17,11 @@ export default function ProfileInnerHeader(props) {
       }}
     >
       <div
-        className="h-[160px] w-[160px] rounded-full bg-cover border-white border-3 absolute left-[20px] top-12"
+        className="h-[160px] md:h-[240px] lg:h-[320px] xl:h-[370px] w-[160px] md:w-[240px] lg:w-[320px] xl:w-[370px] rounded-full bg-cover border-white border-3 absolute left-[20px] md:left-[40px] lg:left-[50px] xl:left-[60px] top-[45px] md:top-[60px] lg:top-[70px] xl:top-[110px]"
         style={{ backgroundImage: profileImageUrl }}
       />
       <button
-        className="w-[30px] h-[30px] bg-white rounded-full flex justify-center items-center my-[10px] hover:cursor-pointer"
+        className="w-[30px] h-[30px] bg-white rounded-full flex justify-center items-center my-[10px] md:my-[20px] hover:cursor-pointer"
         onClick={props.onEditBannerClick}
       >
         <img src={PenIcon} alt="Editar banner" />
