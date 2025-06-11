ead711dc6f4617ad5c61349342360788c77b7ef2
lucas19fonseca <lucas19fonseca@gmail.com>
2025-06-11 14:19:41 -0300

    ajuste tela 2xl

--- src/components/LandingPage/LandingPageBody/index.jsx
+++ src/components/LandingPage/LandingPageBody/index.jsx
@@ -14,14 +14,14 @@ export default function HomeBody() {
             text-5xl mb-5
             md:text-7xl
             xl:text-8xl  
-            2xl:text-9xl ">
+            2xl:text-8xl ">
               Kubo</h1>
 
             <div className="
              text-2xl w-60 
              md:text-4xl md:w-90
              xl:text-5xl xl:w-120
-             2xl:text-7xl 2xl:w-180 2xl:leading-20">
+             2xl:text-6xl 2xl:w-160 2xl:leading-18">
               O maior banco de dados de arquitetura do país
             </div>
           </div>
@@ -35,7 +35,7 @@ export default function HomeBody() {
                 className="w-60 h-40 object-cover rounded-xl absolute top-40 left-17
                  md:top-15 md:left-3 md:h-50 md:w-auto
                  xl:h-60 xl:w-auto xl xl:top-10 xl:left-10
-                 2xl:top-25 2xl:left-20 2xl:w-auto 2xl:h-115 "
+                 2xl:top-10 2xl:left-20 2xl:w-auto 2xl:h-95"
               />
               <img
                 src={ImgOnda}
@@ -43,7 +43,7 @@ export default function HomeBody() {
                 className="w-60 h-40 object-cover rounded-xl absolute top-68 -left-23 z-1
                  md:top-50 md:left-48 md:h-50 md:w-auto 
                  xl:h-60 xl:w-auto xl:top-50 xl:left-65
-                 2xl:w-auto 2xl:h-115 2xl:top-105 2xl:left-130"
+                 2xl:w-auto 2xl:h-95 2xl:top-80 2xl:left-120"
               />
               <img
                 src={ImgPredio}
@@ -51,7 +51,7 @@ export default function HomeBody() {
                 className="w-60 h-40 object-cover rounded-xl absolute top-88 left-5 
                 md:top-80 md:-left-10 md:h-50 md:w-auto 
                 xl:h-60 xl:w-auto xl:top-90 xl:-left-7
-                2xl:w-auto 2xl:h-115 2xl:top-180 2xl:-left-9"
+                2xl:w-auto 2xl:h-95 2xl:top-135 2xl:-left-9"
               />
             </div>
           </div>
@@ -61,7 +61,7 @@ export default function HomeBody() {
             <p className="text-white font-light 
             text-2xl  
             xl:text-3xl
-            2xl:text-5xl">
+            2xl:text-4xl">
               + de <span className="text-blue-400">1000</span> projetos documentados
             </p>
           </div>
