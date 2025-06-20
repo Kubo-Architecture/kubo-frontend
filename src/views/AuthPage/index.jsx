3373ccb66fd508b817b75a46fbef811f7e9c2a15
lucas19fonseca <lucas19fonseca@gmail.com>
2025-06-20 18:39:30 -0300

    tela finalizada

--- src/views/AuthPage/index.jsx
+++ src/views/AuthPage/index.jsx
@@ -126,27 +126,33 @@ const VerificationCodeInput = () => {
         )}
 
         {!limitReached ? (
-          <div className="flex flex-col justify-between min-h-screen md:grid md:grid-cols-2 md:py-4 md:min-w-[1080px]">
+          <div className="flex flex-col justify-between min-h-screen min-w-[382px] 
+          md:grid md:grid-cols-2 md:py-4 md:min-w-[1080px] ">
             {/* lado esquerdo */}
             <div className="flex flex-col justify-between min-h-[100vh] md:min-h-[calc(100vh-32px)]">
-              <div className="space-y-8 md:space-y-10">
-                <div className='w-full flex justify-center md:justify-start md:ml-29 md:mt-3'>
+              <div className="space-y-7 md:space-y-10">
+                <div className='w-full flex justify-center mb-10 mt-5 md:justify-start md:ml-29 md:mt-3'>
                   <SimpleHeader />
                 </div>
 
-                <div className='bg-[#D9D9D9] rounded-r-full flex justify-end md:hidden w-[260px] h-[136px] pr-[60px]'>
+                <div className='bg-[#D9D9D9] rounded-r-full flex justify-end md:hidden w-[260px] h-[136px] pr-[60px] 
+                sm:w-85'>
                   <img src={Aviao} alt="img avião" className='h-[72px] mt-8' />
                 </div>
 
-                <div className='hidden md:block text-5xl w-[480px] md:ml-29'>
+                <div className='text-4xl w-[480px] ml-7
+                sm:ml-20
+                md:text-5xl md:ml-29'>
                   <h3>Verifique seu email</h3>
                 </div>
 
-                <div className="w-[360px] text-2xl ml-7 md:ml-29 md:text-4xl md:font-light md:w-[480px]">
+                <div className="w-[360px] text-2xl ml-7 
+                sm:ml-20
+                md:ml-29 md:text-4xl md:font-light md:w-[480px]">
                   <h1 className='font-Montserrat'>Digite o código de confirmação enviado em seu email</h1>
                 </div>
 
-                <div className="flex gap-4 ml-7 pt-4 md:ml-29 md:pt-0">
+                <div className="flex gap-4 ml-7 pt-4 md:ml-29 md:pt-0 mr-10 sm:ml-20">
                   {code.map((digit, index) => (
                     <input
                       key={index}
@@ -161,7 +167,7 @@ const VerificationCodeInput = () => {
                   ))}
                 </div>
 
-                <div className='w-[240px] md:w-[360px] ml-7 md:ml-29'>
+                <div className='w-[240px] md:w-[360px] mb-10 ml-7 md:ml-29 sm:ml-20'>
                   <a href="#" className='text-[#29435E] font-Montserrat text-xl'>
                     Não recebeu o código?
                   </a>
@@ -190,23 +196,23 @@ const VerificationCodeInput = () => {
         ) : (
 
 
-          <div className="md:py-4 grid grid-cols-2 min-w-[874px]">
+          <div className="md:py-4 md:grid grid-cols-2 md:min-w-[874px]">
             <div className="w-90 font-Montserrat text-3xl text-center flex flex-col min-h-screen pt-6 md:pt-7 h-[100vh]">
               <div className="space-y-6 md:space-y-12 flex flex-col flex-grow"> {/* Adicionei flex-grow aqui */}
-                <div className='w-full flex justify-center md:justify-start md:ml-29'>
+                <div className='w-full flex justify-center ml-6 mb-28 md:mb-10 md:ml-0'>
                   <SimpleHeader />
                 </div>
 
-                <div className='bg-[#D9D9D9] rounded-r-full flex justify-end w-65 h-34 pr-15 md:w-90'>
+                <div className='bg-[#D9D9D9] rounded-r-full flex justify-end w-65 h-34 pr-15 md:w-90 mb-20'>
                   <img src={Aviao} alt="img avião" className='h-18 mt-8' />
                 </div>
 
-                <div className='text-justify  md:ml-29 md:w-89'>
+                <div className='text-justify ml-10 md:ml-29 md:w-89'>
                   <p>Você atingiu o limite de tentativas. Por favor, tente novamente mais tarde.</p>
                 </div>
 
                 {/* Container do botão com mt-auto para fixar na parte inferior */}
-                <div className='mt-auto mb-8 flex justify-center md:justify-start md:ml-39'> {/* Adicionei mt-auto e mb-8 */}
+                <div className='mt-auto mb-8 flex justify-center md:justify-start md:ml-39 ml-12'> {/* Adicionei mt-auto e mb-8 */}
                   <button
                     onClick={resetVerification}
                     className='bg-[#000000b7] hover:bg-black transition-all duration-400 text-white font-Montserrat text-xl px-[120px] py-2 rounded-xl '
