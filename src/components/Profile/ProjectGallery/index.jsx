2da139f0bd9b7516b8209f4e16710b1b5fe3a2e3
PedroDiOliveira <pedro.h.soterio@gmail.com>
2025-06-25 18:05:29 -0300

    refac: ajust gallery bottom margim

--- src/components/Profile/ProjectGallery/index.jsx
+++ src/components/Profile/ProjectGallery/index.jsx
@@ -61,7 +61,7 @@ const ProjectGallery = () => {
 
   return (
     <div className="w-full">
-      <div className="w-full pt-[8px] px-[20px] md:px-[30px] lg:px-[40px] xl:px-[70px]">
+      <div className="w-full mb-[20px] pt-[8px] px-[20px] md:px-[30px] lg:px-[40px] xl:px-[70px]">
         <div className="w-full mb-8">
           <h3 className="text-3xl font-medium text-black text-left">
             Meus Projetos
