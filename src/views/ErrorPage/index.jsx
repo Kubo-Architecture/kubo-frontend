25b83d91ba290920ed99ed192d9d0cab1e10c4b6
lucas19fonseca <lucas19fonseca@gmail.com>
2025-12-29 17:03:10 -0300

    tela de erro refatorada

--- src/views/ErrorPage/index.jsx
+++ src/views/ErrorPage/index.jsx
@@ -1,4 +1,3 @@
-import "./styles.css";
 import { useParams } from "react-router-dom";
 import { SimpleHeader } from "../../components/Universal/SimpleHeader";
 import KuboEmptyIcon from "../../assets/icons/Universal/kubo-empty.svg";
@@ -7,14 +6,23 @@ export default function ErrorPage() {
     const { errorCode } = useParams();
     
     return (
-        <div className="error-page-container">
+        <div className="min-h-[400px] min-w-[340px] flex flex-col h-screen w-full px-[25px] max-[340px]:sticky max-[340px]:overflow-x-scroll">
             <div className="my-[30px]">
                 <SimpleHeader />
             </div>
-            <div className="empty-icon-container">
-                <img className="icon" src={KuboEmptyIcon} alt="Empty icon" />
-                <div className="error-status">
-                    <h4>{errorCode}</h4>
+            <div className="flex-1 flex items-center justify-center relative">
+                <img 
+                    className="w-[200px] h-auto" 
+                    src={KuboEmptyIcon} 
+                    alt="Empty icon" 
+                />
+                <div className="absolute top-1/2 left-[49.1%] -mt-[25px] -ml-[25px] text-black
+                    min-[870px]:left-[48.5%] max-[1280px]:left-[48.5%]
+                    min-[540px]:left-[48%] max-[869px]:left-[48%]
+                    max-[539px]:left-[46%]">
+                    <h4 className="font-['Montserrat'] font-medium tracking-[3px] text-[40px]">
+                        {errorCode}
+                    </h4>
                 </div>
             </div>
         </div>
