3b19f690514f2d082d3146a8c77cd049acd62ce1
Bruno <bcgmeireles@gmail.com>
2025-12-29 09:24:26 -0300

    feat: implements analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import App from './App.jsx'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { inject } from '@vercel/analytics';
+
+inject();
 
 createRoot(document.getElementById('root')).render(
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
