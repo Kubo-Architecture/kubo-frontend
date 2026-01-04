4b6307b1a9cd3f7d3d70cd60c88b1f1201386abe
lucas19fonseca <lucas19fonseca@gmail.com>
2026-01-04 15:38:23 -0300

    ajuste nas telas error page

--- src/views/ErrorPage.tsx
+++ src/views/ErrorPage.tsx
@@ -1,17 +1,23 @@
-import { useParams } from "react-router-dom";
-import KuboEmptyIcon from "../assets/icons/Universal/kubo-empty.svg";
+import HouseError from '../assets/icons/Universal/kubo-empty.svg';
 
 export default function ErrorPage() {
-    const { errorCode } = useParams<string>();
+  const errorCode = window.location.pathname.includes('500') ? '500' : '404';
+  const errorMessage = errorCode === '500' ? 'Internal server error' : 'Page not found';
 
-    return (
-        <div className="error-page-container">
-            <div className="empty-icon-container">
-                <img className="icon" src={KuboEmptyIcon} alt="Empty icon" />
-                <div className="error-status">
-                    <h4>{errorCode}</h4>
-                </div>
-            </div>
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
+      <div className="text-center">
+        {/* Error Icon */}
+        <div className="relative inline-block mb-8">
+          <img src={HouseError} alt="Error icon" className="w-64 h-64" />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <span className="text-6xl font-extralight">{errorCode}</span>
+          </div>
         </div>
-    );
+
+        {/* Error message */}
+        <p className="text-xl text-gray-700">{errorMessage}</p>
+      </div>
+    </div>
+  );
 }
